6eb6f507e6caf010ad75e2e7fb1d0f303008246f
jazminvergara <bk39969267@.com>
2024-04-18 16:30:24 -0700

    changes on api.jsx and also in ImageList.jsx

--- src/api.js
+++ src/api.js
@@ -2,12 +2,15 @@ import axios from 'axios'
 
 const searchImage = async(term) =>{
     const url = ''
-              = 'htpps:/api.unplash.com/search/photos/?client_id'
+              = 'htpps:/api.unplash.com/search/photos/?client_id=MwnjueZW7XqxK50jARkpNo7j6JlTFPErQc5guqJDh5A'
     const response = await axios.get(url,{
         params: {
-            query:'cars'
+            query: term
         }
     })
-    console.log(response)
+    console.log(term)
+    console.log(response.data.results)
+
+    return response.data.results
 }
 export default searchImage
